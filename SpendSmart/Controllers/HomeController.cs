48b1d73baaffa693d090a5b83f2ee3e03de524e4
Alexander Soliven <asoliven26@yahoo.com>
2025-02-22 16:01:51 +0800

    Expenses Logger Build from Scratch

--- SpendSmart/Controllers/HomeController.cs
+++ SpendSmart/Controllers/HomeController.cs
@@ -7,10 +7,12 @@ namespace SpendSmart.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
+    private readonly SpendSmartDbContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, SpendSmartDbContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
@@ -21,6 +23,42 @@ public class HomeController : Controller
     public IActionResult Privacy()
     {
         return View();
+    }
+
+    public IActionResult Expenses()
+    {
+        var allExpenses = _context.Expenses.ToList();
+        var totalExpenses = allExpenses.Sum(price => price.Value);
+        ViewBag.Expenses = totalExpenses;
+
+        return View(allExpenses);
+    }    
+    public IActionResult CreateEditExpense(int? id)
+    {
+        if (id != null)
+        {
+            var expenseInDb = _context.Expenses.SingleOrDefault(expense => expense.Id == id);
+            return View(expenseInDb);
+        }
+
+        return View();
+    }    
+    public IActionResult DeleteExpense(int id)
+    {
+        var expenseInDb = _context.Expenses.SingleOrDefault(expense => expense.Id == id);
+        _context.Expenses.Remove(expenseInDb);
+        _context.SaveChanges();
+        return RedirectToAction("Expenses");
+    }
+    public IActionResult CreateEditExpenseForm(Expense expense)
+    {
+        if (expense.Id == 0) { _context.Expenses.Add(expense); }
+        else { _context.Expenses.Update(expense); }
+
+       
+        _context.SaveChanges();
+
+        return RedirectToAction("Expenses");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
